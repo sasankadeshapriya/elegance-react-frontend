c41c9816886644edfa11f5eeca5d8db9b61b07f2
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2023-10-28 11:26:37 +0530

    add-homepage

---
+++ src/components/common/NavbarDesktop.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NavbarDesktop extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NavbarDesktop
---
+++ src/components/common/NavbarMobile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NavbarMobile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NavbarMobile
